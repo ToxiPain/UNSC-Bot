1fcc15e6e8917dc2738d923cdaa6aef1425a2979
TOXIPAIN <109121365+ToxiPain@users.noreply.github.com>
2023-05-31 04:32:05 -0600

    Update config.js

--- config.js
+++ config.js
@@ -8,31 +8,18 @@ import axios from 'axios'
 import moment from 'moment-timezone' 
 
 global.owner = [
-  ['5219992095479', '👑 Mystic - Creador 👑', true],
-  ['50499698072', '💫 Mystic - Collaborator 1 💫', true],
-  ['51940617554', '💫 Mystic - Collaborator 2 💫', true],
-  ['51996089079', '💫 Mystic - Collaborator 3 💫', true],
-  ['593959425714', '💫 Mystic - Collaborator 4 💫', true],
-  ['50246028932', '💫 Mystic - Collaborator 5 💫', true],  
-  ['5212411719888', '💫 Mystic - Collaborator 6 💫', true],
-  ['5215533827255', '💫 Mystic - Collaborator 7 💫', false],
-  ['593968585283'],
-  ['5219993404349'],
-  ['584125778026'],
-  ['5219991402134'],	
-  ['5492266466080'],
-  ['5219996125657'],
-  ['5218442114446']
+  ['50557418454', '👑 Creador 👑', true],
+  ['5493517038110', '💫  Collaborator 1 💫', true]
 ] 
 
 global.suittag = ['5219993404349'] 
 global.prems = ['51995386439'] 
 
-global.packname = '(☞ﾟ∀ﾟ)☞'
-global.author = '★𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝★'
-global.wm = '★𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝★'
-global.igfg = '★𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝★'
-global.wait = '*[❗] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*'
+global.packname = 'STICKER'
+global.author = '♛ 𝓤𝓝𝓢𝓒-𝓑𝓸𝓽 ♛'
+global.wm = '♛ 𝓤𝓝𝓢𝓒-𝓑𝓸𝓽 ♛'
+global.igfg = '♛ 𝓤𝓝𝓢𝓒-𝓑𝓸𝓽 ♛'
+global.wait = '*⠋⠙⠿⠿⠶⠦⠄ Cargando... Por favor espere un momento*'
 
 global.imagen1 = fs.readFileSync('./Menu2.jpg')
 global.imagen2 = fs.readFileSync('./src/nuevobot.jpg') 
@@ -52,12 +39,12 @@ global.tiempo = d.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', 
 //*****************************
 global.wm2 = `▸ ${dia} ${fecha}\n▸ 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝`
 global.gt = '★𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝★'
-global.mysticbot = '★𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝★'
-global.md = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD'
-global.mysticbot = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD'
-global.waitt = '*[❗] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*'
-global.waittt = '*[❗] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*'
-global.waitttt = '*[❗] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*'
+global.mysticbot = '♛ 𝓤𝓝𝓢𝓒-𝓑𝓸𝓽 ♛'
+global.md = 'https://github.com/ToxiPain'
+global.mysticbot = 'https://github.com/ToxiPain'
+global.waitt = '*⠋⠙⠿⠿⠶⠦⠄ Cargando... Por favor espere un momento*'
+global.waittt = '*⠋⠙⠿⠿⠶⠦⠄ Cargando... Por favor espere un momento*'
+global.waitttt = '*⠋⠙⠿⠿⠶⠦⠄ Cargando... Por favor espere un momento*'
 global.nomorown = '5219993404349'
 global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf']
 global.cmenut = '❖––––––『'
