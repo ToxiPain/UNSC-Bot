1a6c7edb83fc303d79e1e3f02161f77782999c65
TOXIPAIN <109121365+ToxiPain@users.noreply.github.com>
2023-05-31 04:50:45 -0600

    Update _antiprivnew.js

--- plugins/_antiprivnew.js
+++ plugins/_antiprivnew.js
@@ -9,7 +9,7 @@ let chat = global.db.data.chats[m.chat]
 let user = global.db.data.users[m.sender]
 let bot = global.db.data.settings[this.user.jid] || {}
 if (bot.antiPrivate && !isOwner && !isROwner) {
-await m.reply(`Hola *@${m.sender.split`@`[0]}*, Esta prohibido usar el bot el privado\n\nUnete a nuestra comunidad: ...`, false, { mentions: [m.sender] })
+await m.reply(`Hola *@${m.sender.split`@`[0]}*, Esta prohibido usar el bot el privado\n\nUnete a nuestra comunidad: https://chat.whatsapp.com/JT9ghLiMHyHAUuLzJPwPzT`, false, { mentions: [m.sender] })
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'banchat')
 return !1
 }}
